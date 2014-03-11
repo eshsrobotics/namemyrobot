b3db538e27cbbf500c3119aeb0c3dfd5fe336445
Zach Latta <zach@zachlatta.com>
2014-03-11 00:15:08 -0700

    Set up Gorp correctly.

--- routes/names.go
+++ routes/names.go
@@ -2,14 +2,14 @@ package routes
 
 import "github.com/coopernurse/gorp"
 
-func GetFirstNames(db *gorp.SqlExecutor) (int, string) {
+func GetFirstNames(db gorp.SqlExecutor) (int, string) {
 	return 0, ""
 }
 
-func GetMiddleNames(db *gorp.SqlExecutor) (int, string) {
+func GetMiddleNames(db gorp.SqlExecutor) (int, string) {
 	return 0, ""
 }
 
-func GetLastNames(db *gorp.SqlExecutor) (int, string) {
+func GetLastNames(db gorp.SqlExecutor) (int, string) {
 	return 0, ""
 }
--- server.go
+++ server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/codegangsta/martini"
+	"github.com/coopernurse/gorp"
+	"github.com/eshsrobotics/namemyrobot/models"
 	"github.com/eshsrobotics/namemyrobot/routes"
 )
 
@@ -9,6 +11,7 @@ func main() {
 	m := martini.Classic()
 
 	m.Use(martini.Static("public"))
+	m.MapTo(models.Dbm, (*gorp.SqlExecutor)(nil))
 
 	m.Get(`/namemyrobot/first_names`, routes.GetFirstNames)
 	m.Get(`/namemyrobot/middle_names`, routes.GetLastNames)
